f922887348a2406e3274b9b79cd4414ca099bdc6
jvdpt0 <jvdpt0@gmail.com>
2023-07-05 23:57:09 -0300

    Multiple changes
    Added index page, all posts and individual post pages with dummy content, 404 page and styling

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name='index'),
     path('posts', views.all_posts, name='all-posts'),
-    path('posts/<slug:post>', views.post, name='post')
+    path('posts/<slug:slug>', views.post, name='post')
 ]
